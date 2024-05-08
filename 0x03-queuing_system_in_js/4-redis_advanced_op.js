2570ba3e701425a2f409be79a81a1c9d7eb1cb86
Wangui-quinnie <w.wangui@ymail.com>
2024-05-08 12:31:36 +0000

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,38 @@
+import redis from 'redis';
+
+// Create a Redis client
+const client = redis.createClient();
+
+// Handle Redis connection events
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
+
+// Function to create a hash with hset
+function createHash() {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+}
+
+// Function to display the hash with hgetall
+function displayHash() {
+  client.hgetall('HolbertonSchools', (error, reply) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+// Call the functions
+createHash();
+displayHash();
