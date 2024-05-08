52bfe0b5201a6db50f3290de379f9932a555a30b
Wangui-quinnie <w.wangui@ymail.com>
2024-05-08 15:57:30 +0000

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,31 @@
+import kue from 'kue';
+
+// Create a job queue named push_notification_code
+const queue = kue.createQueue();
+
+// Create an object containing the Job data
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'Hello, this is a notification message!'
+};
+
+// Create a job with the provided data
+const job = queue.create('push_notification_code', jobData);
+
+// When the job is created without error
+job.on('enqueue', () => {
+  console.log(`Notification job created: ${job.id}`);
+});
+
+// When the job is completed
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+// When the job fails
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
+
+// Save the job to the queue
+job.save();
